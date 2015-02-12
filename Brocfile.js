2671308a04d1372f9592553f3db46b29f7be10f7
Artur Khantimirov <yokree@gmail.com>
2015-02-12 23:05:32 +1300

    add d3.js

--- Brocfile.js
+++ Brocfile.js
@@ -5,6 +5,8 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp();
 
 app.import('bower_components/bootstrap-sass/assets/javascripts/bootstrap.js');
+app.import('bower_components/d3/d3.js');
+
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
