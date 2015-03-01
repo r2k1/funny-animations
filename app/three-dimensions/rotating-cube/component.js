8daf2052c91f2a0cd32c6a1b47d8a59cc6597b62
Artur Khantimirov <yokree@gmail.com>
2015-03-01 14:50:35 +1300

    Set rotating cube size based on parent width and height

--- app/three-dimensions/rotating-cube/component.js
+++ app/three-dimensions/rotating-cube/component.js
@@ -4,19 +4,19 @@ import CubeFactory from "funny-animations/models/cube-factory";
 var RotatingCube = Ember.Component.extend({
   tagName:   'svg',
   container: null,
-  width:     1000,
-  height:    600,
   camera:    mat4.create(),
   cube:      null,
 
   didInsertElement: function () {
+    var width = this.$().parent().width();
+    var height = this.$().parent().height();
     this.container = d3.select(this.element)
-      .attr('width', this.width)
-      .attr('height', this.height)
+      .attr('width', width)
+      .attr('height',height)
       .append('g');
-    var cubeLength = Math.min(this.width, this.height) / 2 / Math.sin(Math.PI / 3);
+    var cubeLength = Math.min(width, height) / 2 / Math.sin(Math.PI / 3);
     this.set('cube', CubeFactory.create().build({
-      vector:    vec4.fromValues(this.width / 2, this.height / 2, 0, cubeLength),
+      vector:    vec4.fromValues(width / 2, height / 2, 0, cubeLength),
       container: this.container,
       camera:    this.get('camera')
     }));
