a537319b5299245c2c5d5c021e29f964f6d1c3fc
Artur Khantimirov <yokree@gmail.com>
2015-03-07 15:54:33 +1300

    show camera matrix

--- app/components/matrix-component/component.js
+++ app/components/matrix-component/component.js
@@ -3,7 +3,9 @@ import Ember from "ember";
 var MatrixComponent = Ember.Component.extend({
   matrix: null,
   array: function () {
-    var array = Array.prototype.slice.call(this.get('matrix'));
+    var array = Array.prototype.map.call(this.get('matrix'), function(value) {
+      return Math.round(value * 100) / 100;
+    });
     var length = Math.sqrt(array.length);
     var anotherArray = [];
     while(array.length) { anotherArray.push(array.splice(0,length)); }
