6e6854573b02bc0506d211501485130cede1cb1f
Artur Khantimirov <yokree@gmail.com>
2015-03-10 21:51:57 +1300

    add svg element component

---
+++ app/components/svg-element/component.js
@@ -0,0 +1,32 @@
+import Ember from "ember";
+
+export default Ember.Component.extend({
+  width: null,
+  height: null,
+  classNames: ['svg-container'],
+  // emblem doesn't support svg tags
+  template: Ember.Handlebars.compile('<svg>{{yield}}</svg>'),
+
+  setResize: function() {
+    this.resize();
+    this.$(window).resize(this.resize.bind(this));
+  }.on('didInsertElement'),
+
+  // can't set size of svg with css
+  // use all available space in container
+  resize: function() {
+    var width = this.$(this.element).innerWidth();
+    var height = this.$(this.element).innerHeight();
+    this.setProperties({
+      width: width,
+      height: height
+    });
+    d3.select(this.element).select('svg')
+      .attr('width', width)
+      .attr('height', height);
+  },
+
+  removeResizeHandler: function() {
+    this.$(window).off('resize');
+  }.on('willClearRender')
+});
